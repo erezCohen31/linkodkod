f91988277f073ebb85a6d0dbb02a56925b0b5ea6
erez cohen <erezjbh@gmail.com>
2025-09-07 12:30:13 +0300

    add responsive to container post and take from json

--- front-linkodkod/src/App.tsx
+++ front-linkodkod/src/App.tsx
@@ -1,66 +1,10 @@
 import "./App.css";
 import PostsPage from "./pages/Posts.page.tsx";
-const posts = [
-  {
-    url: "../src/assets/imagePost/men.jpg",
-    alt: "image of men",
-    description:
-      "this is a description <eqgwdrghdwrth es<gdhdrh egs<eg QSFQSF Q fQF ",
-    numOfLike: 25,
-    username: "erez",
-    time: "20:00",
-  },
-  {
-    url: "../src/assets/imagePost/men.jpg",
-    alt: "image of men",
-    description:
-      "this is a description <eqgwdrghdwrth es<gdhdrh egs<eg QSFQSF Q fQF ",
-    numOfLike: 25,
-    username: "erez",
-    time: "20:00",
-  },
-  {
-    url: "../src/assets/imagePost/men.jpg",
-    alt: "image of men",
-    description:
-      "this is a description <eqgwdrghdwrth es<gdhdrh egs<eg QSFQSF Q fQF ",
-    numOfLike: 25,
-    username: "erez",
-    time: "20:00",
-  },
-  {
-    url: "../src/assets/imagePost/men.jpg",
-    alt: "image of men",
-    description:
-      "this is a description <eqgwdrghdwrth es<gdhdrh egs<eg QSFQSF Q fQF ",
-    numOfLike: 25,
-    username: "erez",
-    time: "20:00",
-  },
-  {
-    url: "../src/assets/imagePost/men.jpg",
-    alt: "image of men",
-    description:
-      "this is a description <eqgwdrghdwrth es<gdhdrh egs<eg QSFQSF Q fQF ",
-    numOfLike: 25,
-    username: "erez",
-    time: "20:00",
-  },
-  {
-    url: "../src/assets/imagePost/men.jpg",
-    alt: "image of men",
-    description:
-      "this is a description <eqgwdrghdwrth es<gdhdrh egs<eg QSFQSF Q fQF ",
-    numOfLike: 25,
-    username: "erez",
-    time: "20:00",
-  },
-];
 
 function App() {
   return (
     <>
-      <PostsPage posts={posts} />
+      <PostsPage />
     </>
   );
 }

3ea1a8591557d6651760cab9ab0e73f6451988e4
erez cohen <erezjbh@gmail.com>
2025-09-07 17:50:18 +0300

    add responsive to header

--- front-linkodkod/src/components/application_layout/Header.tsx
+++ front-linkodkod/src/components/application_layout/Header.tsx
@@ -3,8 +3,8 @@ import "../../style/Header.css";
 export default function Header() {
   return (
     <div className="header">
-      <h1>you protect us we bind you</h1>
       <div className="logo"></div>
+      <h1>you protect us we bind you</h1>
     </div>
   );
 }

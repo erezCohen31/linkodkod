128d7f7e797807dc93e0f06053492b232b4ce20b
erez cohen <erezjbh@gmail.com>
2025-09-07 14:51:24 +0300

    chnege yhe color like onclick

--- front-linkodkod/src/components/Post.tsx
+++ front-linkodkod/src/components/Post.tsx
@@ -1,14 +1,23 @@
+import { useRef, useState } from "react";
 import type PostProps from "../interface/PostProps.ts";
 import "../style/Post.css";
 
 export default function Post({ post }: PostProps) {
+  const [likeState, useLikeState] = useState("like");
   return (
     <div className="post">
       <img className="post-image" src={post.url} alt={post.alt} />
       <p>Description:{post.description}</p>
       <div className="post-information">
         <div className="container-like">
-          <span className="material-symbols-outlined">favorite</span>
+          <div
+            className={likeState}
+            onClick={() =>
+              useLikeState(
+                likeState === "like-clicked" ? "like" : "like-clicked"
+              )
+            }
+          ></div>
           <p>{post.numOfLike}</p>
         </div>
         <p>{post.username}</p>

c4f650590fc461bfa4e3c138edf852f0548322e5
erez cohen <erezjbh@gmail.com>
2025-09-09 12:12:39 +0300

    change the time

--- server/service/postService.js
+++ server/service/postService.js
@@ -6,6 +6,18 @@ function findId() {
   const id = Math.max(...posts.map((o) => o.id));
   return id;
 }
+function getTime() {
+  const timestamp = Date.now();
+  const dateObject = new Date(timestamp);
+  const dateString = dateObject.toLocaleString("fr-Fr", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return dateString;
+}
 const postService = {
   getAllPost() {
     const posts = readFile(postPath);
@@ -25,7 +37,7 @@ const postService = {
       description: description,
       numOfLike: 0,
       username: "erez",
-      time: Date.now(),
+      time: getTime(),
     };
     writeFile(postPath, newPost);
   },

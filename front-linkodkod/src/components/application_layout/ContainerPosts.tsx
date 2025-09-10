87780c77dced2f598217520a01a7b41a6da17085
erez cohen <erezjbh@gmail.com>
2025-09-10 17:19:54 +0300

    add input search

--- front-linkodkod/src/components/application_layout/ContainerPosts.tsx
+++ front-linkodkod/src/components/application_layout/ContainerPosts.tsx
@@ -4,11 +4,14 @@ import { useContext, useEffect, useState } from "react";
 import type PostType from "../../interface/Post.ts";
 import { UserContext } from "../../context/User.context.tsx";
 import { getAllPosts } from "../../controller/PostController.ts";
+import { useNavigate } from "react-router";
 
 export default function PostsPage() {
   const [posts, setPosts] = useState<PostType[]>();
   const [error, setError] = useState<any>();
   const { user } = useContext(UserContext);
+  const [postId, setPostId] = useState();
+  const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
   {
@@ -38,6 +41,25 @@ export default function PostsPage() {
     posts && (
       <div className="home">
         <h1>Welcome {user?.name}</h1>
+        <form className="input-search-post">
+          <input
+            id="text"
+            name="text"
+            type="text"
+            value={postId}
+            onChange={(e: any) => setPostId(e.target.value)}
+            placeholder="enter your post id"
+            required
+          />
+          <button
+            onClick={() => {
+              navigate(`../post/${postId}`);
+            }}
+          >
+            search
+          </button>
+        </form>
+
         <div className="container-posts">
           {posts?.map((post) => (
             <Post key={post.id} post={post} />
